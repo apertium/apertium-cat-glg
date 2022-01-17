19dd7ac3da4a3df003afd12fcfcc93d14ec069fb
Marc Riera Irigoyen <marc.riera.irigoyen@gmail.com>
2022-01-17 16:50:27 +0100

    Update testvoc scripts, apertium-dixtools formatting

--- dev/testvoc/inconsistency-summary.sh
+++ dev/testvoc/inconsistency-summary.sh
@@ -1,67 +1,43 @@
+#!/bin/bash
+
+IFS=","
 INC=$1
 PAIR=$2
 OUT=testvoc-summary.$PAIR.txt
-POS="abbr adj adv cm cnjadv cnjcoo cnjsub det guio ij n np num pr preadv prn rel vaux vbhaver vblex vbser vbmod predet"
+POS=($(grep -m 1 "^POS=" testvoc.conf | cut -d = -f 2))
+unset IFS
 
 echo -n "" > $OUT;
 
 date >> $OUT
-echo -e "===============================================" >> $OUT
+echo -e "================================================" >> $OUT
 echo -e "POS\tTotal\tClean\tWith @\tWith #\tClean %" >> $OUT
-for i in $POS; do
-	if [ "$i" = "det" ]; then
-		TOTAL=`cat $INC | grep "<$i>" | grep -v -e '<n>' -e '<np>' | grep -v REGEX | wc -l`; 
-		AT=`cat $INC | grep "<$i>" | grep '@' | grep -v -e '<n>' -e '<np>'  | grep -v REGEX | wc -l`;
-		HASH=`cat $INC | grep "<$i>" | grep '>  *#' | grep -v -e '<n>' -e '<np>' | grep -v REGEX |  wc -l`;
-	elif [ "$i" = "preadv" ]; then
-		TOTAL=`cat $INC | grep "<$i>" | grep -v -e '<adj>' -e '<adv>' | grep -v REGEX | wc -l`; 
-		AT=`cat $INC | grep "<$i>" | grep '@' | grep -v -e '<adj>' -e '<adv>'  | grep -v REGEX | wc -l`;
-		HASH=`cat $INC | grep "<$i>" | grep '>  *#' | grep -v -e '<adj>' -e '<adv>' | grep -v REGEX |  wc -l`;
-	elif [ "$i" = "adv" ]; then
-		TOTAL=`cat $INC | grep "<$i>" | grep -v -e '<adj>' -e '<v' | grep -v REGEX | wc -l`; 
-		AT=`cat $INC | grep "<$i>" | grep '@' | grep -v -e '<adj>' -e '<v' | grep -v REGEX | wc -l`;
-		HASH=`cat $INC | grep "<$i>" | grep '>  *#' | grep -v -e '<adj>' -e '<v' | grep -v REGEX |  wc -l`;
-	elif [ "$i" = "cnjsub" ]; then
-		TOTAL=`cat $INC | grep "<$i>" | grep -v -e '<v' | grep -v REGEX | wc -l`; 
-		AT=`cat $INC | grep "<$i>" | grep '@' | grep -v  -e '<v' | grep -v REGEX | wc -l`;
-		HASH=`cat $INC | grep "<$i>" | grep '>  *#' | grep -v -e '<v' | grep -v REGEX |  wc -l`;
-	elif [ "$i" = "prn" ]; then
-		TOTAL=`cat $INC | grep "<$i>" | grep -v -e '<pr>' -e '<v' | grep -v REGEX | wc -l`; 
-		AT=`cat $INC | grep "<$i>" | grep '@' | grep -v  -e '<pr>' -e '<v' | grep -v REGEX | wc -l`;
-		HASH=`cat $INC | grep "<$i>" | grep '>  *#' | grep -v -e '<pr>' -e '<v' | grep -v REGEX |  wc -l`;
-	elif [ "$i" = "vbhaver" ]; then
-		TOTAL=`cat $INC | grep "<$i>" | grep -v -e '<pp' | grep -v REGEX | wc -l`; 
-		AT=`cat $INC | grep "<$i>" | grep '@' | grep -v  -e '<pp' | grep -v REGEX | wc -l`;
-		HASH=`cat $INC | grep "<$i>" | grep '>  *#' | grep -v -e '<pp' | grep -v REGEX |  wc -l`;
-	elif [ "$i" = "vblex" ]; then
-		TOTAL=`cat $INC | grep "<$i>" | grep -v -e '<adv' | grep -v REGEX | wc -l`; 
-		AT=`cat $INC | grep "<$i>" | grep '@' | grep -v  -e '<adv' | grep -v REGEX | wc -l`;
-		HASH=`cat $INC | grep "<$i>" | grep '>  *#' | grep -v -e '<adv' | grep -v REGEX |  wc -l`;
-	elif [ "$i" = "pr" ]; then
-		TOTAL=`cat $INC | grep "<$i>" | grep -v -e '<prn' -e '<ger' -e '<det' | grep -v REGEX | wc -l`; 
-		AT=`cat $INC | grep "<$i>" | grep '@' | grep -v  -e '<prn' -e '<ger' -e '<det' | grep -v REGEX | wc -l`;
-		HASH=`cat $INC | grep "<$i>" | grep '>  *#' | grep -v -e '<prn' -e '<ger' -e '<det' | grep -v REGEX |  wc -l`;
-	elif [ "$i" = "rel" ]; then
-		TOTAL=`cat $INC | grep "<$i>" | grep -v -e '<pr' | grep -v REGEX | wc -l`; 
-		AT=`cat $INC | grep "<$i>" | grep '@' | grep -v  -e '<pr' | grep -v REGEX | wc -l`;
-		HASH=`cat $INC | grep "<$i>" | grep '>  *#' | grep -v -e '<pr' | grep -v REGEX |  wc -l`;
+for i in "${!POS[@]}"; do
+	CONFIG=$(grep -m 1 "^Exclude_${POS[$i]}=" testvoc.conf | cut -d = -f 2)
+	if ! [ -z "$CONFIG" ]; then
+		CONFIG=$(sed "s/,/ -e /g" <<< $CONFIG)
+		ALL=$(cat $INC | grep "<${POS[$i]}>" | grep -v REGEX | grep -v -e $CONFIG);
+		TOTAL=$(cat $INC | grep "<${POS[$i]}>" | grep -v REGEX | grep -v -e $CONFIG | wc -l);
+		AT=$(cat $INC | grep "<${POS[$i]}>" | grep -v REGEX | grep -v -e $CONFIG | grep ' \\@' | wc -l);
+		HASH=$(cat $INC | grep "<${POS[$i]}>" | grep -v REGEX | grep -v -e $CONFIG | grep ' #' | wc -l);
 	else
-		TOTAL=`cat $INC | grep "<$i>" | grep -v REGEX | wc -l`; 
-		AT=`cat $INC | grep "<$i>" | grep '@'  | grep -v REGEX | wc -l`;
-		HASH=`cat $INC | grep "<$i>" | grep '>  *#' | grep -v REGEX |  wc -l`;
+		ALL=$(cat $INC | grep "<${POS[$i]}>" | grep -v REGEX);
+		TOTAL=$(cat $INC | grep "<${POS[$i]}>" | grep -v REGEX | wc -l);
+		AT=$(cat $INC | grep "<${POS[$i]}>" | grep -v REGEX | grep ' \\@' | wc -l);
+		HASH=$(cat $INC | grep "<${POS[$i]}>" | grep -v REGEX | grep ' #' | wc -l);
 	fi
-	UNCLEAN=`calc $AT+$HASH`;
-	CLEAN=`calc $TOTAL-$UNCLEAN`;
-	PERCLEAN=`calc $UNCLEAN/$TOTAL*100 |sed 's/^\W*//g' | sed 's/~//g' | head -c 5`;
+	UNCLEAN=`bc <<< $AT+$HASH`;
+	CLEAN=`bc <<< $TOTAL-$UNCLEAN`;
+	PERCLEAN=`calc $UNCLEAN/$TOTAL*100 | sed 's/^\W*//g' | sed 's/~//g' | head -c 5`;
 	echo $PERCLEAN | grep "Err" > /dev/null;
 	if [ $? -eq 0 ]; then
 		TOTPERCLEAN="100";
 	else
-		TOTPERCLEAN=`calc 100-$PERCLEAN | sed 's/^\W*//g' | sed 's/~//g' | head -c 5`;
+		TOTPERCLEAN=`echo 100-$PERCLEAN | bc | sed 's/^\W*//g' | sed 's/~//g' | head -c 5`;
 	fi
 
-	echo -e $TOTAL";"$i";"$CLEAN";"$AT";"$HASH";"$TOTPERCLEAN;
+	if [ $TOTAL -gt 0 ]; then echo -e $TOTAL";"${POS[$i]}";"$CLEAN";"$AT";"$HASH";"$TOTPERCLEAN; fi
 done | sort -gr | awk -F';' '{print $2"\t"$1"\t"$3"\t"$4"\t"$5"\t"$6}' >> $OUT
 
-echo -e "===============================================" >> $OUT
+echo -e "================================================" >> $OUT
 cat $OUT;
--- dev/testvoc/testvoc.sh
+++ dev/testvoc/testvoc.sh
@@ -1,5 +1,61 @@
-echo "==Catalan->Galician==========================";
-bash inconsistency.sh cat-glg ../../../../languages/apertium-cat/apertium-cat.cat.dix > /tmp/cat-glg.testvoc; sh inconsistency-summary.sh /tmp/cat-glg.testvoc cat-glg 
-echo ""
-echo "==Galician->Catalan===========================";
-bash inconsistency.sh glg-cat ../../../../languages/apertium-glg/apertium-glg.glg.dix > /tmp/glg-cat.testvoc; bash inconsistency-summary.sh /tmp/glg-cat.testvoc glg-cat
+#!/bin/bash
+
+if ! [[ -e testvoc.conf ]]; then
+    echo "Testvoc configuration file (testvoc.conf) not found."
+    exit 1
+fi
+
+TRIMMED=true
+
+while getopts "equt" opt; do
+  case $opt in
+    e)
+      ENCLITICS=true  # If the -e flag is used, enclitics are skipped for faster processing
+      ;;
+    q)
+      QUIET=true  # If the -q flag is used, no summary is generated
+      ;;
+    u)
+      UNKNOWNS=true  # If the -u flag is used, unknown words are checked
+      ;;
+    t)
+      TRIMMED=false  # If the -t flag is used, the source monodix is not trimmed
+      ;;
+  esac
+done
+
+IFS=","
+modes=($(grep -m 1 "^PairModes=" testvoc.conf | cut -d = -f 2))
+modenames=($(grep -m 1 "^PairModeNames=" testvoc.conf | cut -d = -f 2))
+langs=($(grep -m 1 "^PairLangs=" testvoc.conf | cut -d = -f 2))
+langnames=($(grep -m 1 "^PairLangNames=" testvoc.conf | cut -d = -f 2))
+unset IFS
+
+for i in "${!modes[@]}"; do
+    printf "== %.45s\n" "${modenames[$i]} ============================================"
+    if [[ $ENCLITICS = true ]] && [[ $TRIMMED = false ]]; then
+        bash inconsistency.sh -et ${modes[$i]} auto > .testvoc
+    elif [[ $ENCLITICS = true ]]; then
+        bash inconsistency.sh -e ${modes[$i]} auto > .testvoc
+    elif [[ $TRIMMED = false ]]; then
+        bash inconsistency.sh -t ${modes[$i]} auto > .testvoc
+    else
+        bash inconsistency.sh ${modes[$i]} auto > .testvoc
+    fi
+    grep -vP '(?!\\)\/.*   --------->   [^#].*\\\/' .testvoc | grep -vP '^\^@.*   --------->   \\@.*' | grep -e ' #' -e '\\\/' -e ' \\@' > testvoc-errors.${modes[$i]}.txt
+
+    if ! [[ $QUIET ]]; then
+        bash inconsistency-summary.sh .testvoc ${modes[$i]}
+    fi
+    rm .testvoc
+done
+
+if [[ $UNKNOWNS ]]; then
+    for i in "${!langs[@]}"; do
+        printf "== %.45s\n" "${langnames[$i]} ============================================"
+        pushd ../../ > /dev/null; bash dev/testvoc/bidix-unknowns.sh ${langs[$i]} | grep -v ":<:" | grep -v "<prn><enc>" > dev/testvoc/testvoc-missing.${langs[$i]}.txt; popd > /dev/null;
+        printf "%s\n" "Missing entries: $(cat testvoc-missing.${langs[$i]}.txt | wc -l)"
+    done
+fi
+
+exit 0
